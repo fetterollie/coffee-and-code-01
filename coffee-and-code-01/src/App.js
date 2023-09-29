5745cfad8b5d511723c59b7291a189e60b738e5e
fetterollie <jonathan.fetterolf@gmail.com>
2023-09-29 17:07:30 -0400

    weather api

--- coffee-and-code-01/src/App.js
+++ coffee-and-code-01/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [location, setLocation] = useState(null);
+  const [weather, setWeather] = useState(null);
+  // const apiKey = process.env.REACT_APP_API_KEY;
+  const [icon, setIcon] = useState("http://openweathermap.org/img/w/10d.png")
+  const [units, setUnits] = useState("imperial")
+
+  function handleLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      console.log("Can't get geolocation");
+    }
+  }
+
+  function toggleUnit() {
+    if (units === "imperial") {
+      setUnits("metric");
+    } else {
+      setUnits("imperial");
+    }
+  }
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    setLocation({ latitude, longitude, units });
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+
+    // Make API call to OpenWeatherMap
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}&units=${units}`)
+      .then(response => response.json())
+      .then(data => {
+        setWeather(data);
+        // console.log("check: ", data.weather[0].icon);
+        setIcon(`http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+        console.log(data);
+      })
+      .catch(error => console.log(error));
+  }
+
+  function error() {
+    console.log("Unable to get your location");
+  }
+
+  useEffect(() => {
+    handleLocation()
+  }, [units]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather</h1>
+      <button onClick={toggleUnit}>Toggle Units</button>
+      {weather ? (
+        <div>
+          <p>Location: {weather.name}</p>
+          <p>Temperature: {weather.main.temp}</p>
+          <p>Weather: {weather.weather[0].description}</p>
+          <img src={icon}/>
+        </div>
+      ) : null}
     </div>
   );
 }
